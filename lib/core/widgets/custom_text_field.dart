8b65b392bc1e255dc5f7871784136fd200477b24
dahamkakooza <157519993+dahamkakooza@users.noreply.github.com>
2025-11-17 23:01:24 +0300

    Add CustomTextField and related text field classes

---
+++ lib/core/widgets/custom_text_field.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import '../theme/colors.dart';
+import '../theme/text_styles.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final bool enabled;
+  final String? errorText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final int maxLines;
+  final int? maxLength;
+  final void Function(String)? onChanged;
+  final void Function(String)? onSubmitted;
+  final String? Function(String?)? validator;
+  final TextInputAction textInputAction;
+  final bool autocorrect;
+  final bool enableSuggestions;
+  final FocusNode? focusNode;
+  final bool readOnly;
+  final VoidCallback? onTap;
+  final String? initialValue;
+  final EdgeInsetsGeometry? contentPadding;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.enabled = true,
+    this.errorText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.maxLines = 1,
+    this.maxLength,
+    this.onChanged,
+    this.onSubmitted,
+    this.validator,
+    this.textInputAction = TextInputAction.next,
+    this.autocorrect = true,
+    this.enableSuggestions = true,
+    this.focusNode,
+    this.readOnly = false,
+    this.onTap,
+    this.initialValue,
+    this.contentPadding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyles.titleSmall.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+          enabled: enabled,
+          maxLines: maxLines,
+          maxLength: maxLength,
+          onChanged: onChanged,
+          onFieldSubmitted: onSubmitted,
+          validator: validator,
+          textInputAction: textInputAction,
+          autocorrect: autocorrect,
+          enableSuggestions: enableSuggestions,
+          focusNode: focusNode,
+          readOnly: readOnly,
+          onTap: onTap,
+          initialValue: initialValue,
+          decoration: InputDecoration(
+            hintText: hintText,
+            prefixIcon: prefixIcon,
+            suffixIcon: suffixIcon,
+            errorText: errorText,
+            counterText: '',
+            contentPadding: contentPadding ?? const EdgeInsets.all(16),
+            hintStyle: TextStyles.bodyMedium.copyWith(
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+            ),
+          ),
+          style: TextStyles.bodyMedium.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class PasswordTextField extends StatefulWidget {
+  final String label;
+  final TextEditingController? controller;
+  final String? errorText;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final TextInputAction textInputAction;
+  final FocusNode? focusNode;
+  final bool enabled;
+
+  const PasswordTextField({
+    super.key,
+    required this.label,
+    this.controller,
+    this.errorText,
+    this.onChanged,
+    this.validator,
+    this.textInputAction = TextInputAction.next,
+    this.focusNode,
+    this.enabled = true,
+  });
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool _obscureText = true;
+
+  void _toggleObscureText() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(
+      label: widget.label,
+      controller: widget.controller,
+      obscureText: _obscureText,
+      errorText: widget.errorText,
+      onChanged: widget.onChanged,
+      validator: widget.validator,
+      textInputAction: widget.textInputAction,
+      focusNode: widget.focusNode,
+      enabled: widget.enabled,
+      suffixIcon: IconButton(
+        icon: Icon(
+          _obscureText ? Icons.visibility : Icons.visibility_off,
+          color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5),
+        ),
+        onPressed: _toggleObscureText,
+      ),
+    );
+  }
+}
+
+class SearchTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String hintText;
+  final void Function(String)? onChanged;
+  final VoidCallback? onTap;
+  final bool enabled;
+
+  const SearchTextField({
+    super.key,
+    this.controller,
+    this.hintText = 'Search...',
+    this.onChanged,
+    this.onTap,
+    this.enabled = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      onChanged: onChanged,
+      onTap: onTap,
+      enabled: enabled,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: const Icon(Icons.search),
+        contentPadding: const EdgeInsets.symmetric(vertical: 12),
+      ),
+    );
+  }
+}
+
+class MultilineTextField extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final TextEditingController? controller;
+  final String? errorText;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final int maxLines;
+  final int? maxLength;
+
+  const MultilineTextField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.controller,
+    this.errorText,
+    this.onChanged,
+    this.validator,
+    this.maxLines = 4,
+    this.maxLength,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(
+      label: label,
+      hintText: hintText,
+      controller: controller,
+      errorText: errorText,
+      onChanged: onChanged,
+      validator: validator,
+      maxLines: maxLines,
+      maxLength: maxLength,
+      keyboardType: TextInputType.multiline,
+    );
+  }
+}
