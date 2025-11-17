4876d17acfb4881b571110d75c7f12fabfc7da58
dahamkakooza <157519993+dahamkakooza@users.noreply.github.com>
2025-11-17 23:24:30 +0300

    Implement ProfileLocalDataSource for user preferences

---
+++ lib/features/profile/data/datasources/profile_local_data_source.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/user_preferences_model.dart';
+
+abstract class ProfileLocalDataSource {
+  Future<UserPreferencesModel> getPreferences();
+  Future<void> savePreferences(UserPreferencesModel preferences);
+  Future<void> clearPreferences();
+}
+
+class ProfileLocalDataSourceImpl implements ProfileLocalDataSource {
+  final SharedPreferences sharedPreferences;
+  static const String _preferencesKey = 'user_preferences';
+
+  ProfileLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<UserPreferencesModel> getPreferences() async {
+    try {
+      final jsonString = sharedPreferences.getString(_preferencesKey);
+      if (jsonString == null) {
+        return UserPreferencesModel.defaultPreferences;
+      }
+      
+      final jsonMap = json.decode(jsonString) as Map<String, dynamic>;
+      return UserPreferencesModel.fromJson(jsonMap);
+    } catch (e) {
+      // If there's any error reading preferences, return defaults
+      return UserPreferencesModel.defaultPreferences;
+    }
+  }
+
+  @override
+  Future<void> savePreferences(UserPreferencesModel preferences) async {
+    try {
+      final jsonString = json.encode(preferences.toJson());
+      await sharedPreferences.setString(_preferencesKey, jsonString);
+    } catch (e) {
+      throw Exception('Failed to save preferences: $e');
+    }
+  }
+
+  @override
+  Future<void> clearPreferences() async {
+    try {
+      await sharedPreferences.remove(_preferencesKey);
+    } catch (e) {
+      throw Exception('Failed to clear preferences: $e');
+    }
+  }
+
+  // Additional utility methods
+  Future<void> saveThemeMode(ThemeMode themeMode) async {
+    final currentPreferences = await getPreferences();
+    final updatedPreferences = currentPreferences.copyWith(themeMode: themeMode);
+    await savePreferences(updatedPreferences);
+  }
+
+  Future<void> saveNotificationsEnabled(bool enabled) async {
+    final currentPreferences = await getPreferences();
+    final updatedPreferences = currentPreferences.copyWith(notificationsEnabled: enabled);
+    await savePreferences(updatedPreferences);
+  }
+
+  Future<void> saveLanguage(Language language) async {
+    final currentPreferences = await getPreferences();
+    final updatedPreferences = currentPreferences.copyWith(language: language);
+    await savePreferences(updatedPreferences);
+  }
+
+  Future<void> saveBiometricAuthEnabled(bool enabled) async {
+    final currentPreferences = await getPreferences();
+    final updatedPreferences = currentPreferences.copyWith(biometricAuthEnabled: enabled);
+    await savePreferences(updatedPreferences);
+  }
+}
